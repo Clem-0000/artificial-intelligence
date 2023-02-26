748176f9d4c88ccc5808d18a2eb87ca5d71d03fe
Clem-0000 <clemtoon78700.bri@gmail.com>
2023-02-26 18:10:34 +0100

    TP5

--- main.py
+++ main.py
@@ -40,6 +40,13 @@ def etat_final(actions):
 
 
 def minmax(actions, joueur):
+    """
+    Question 3
+    Fonction testant pour chaque case la valeur de la fonction d'évaluation et renvoie en fonction du joueur cette valeur.
+    :param actions: Contient une version de la grille courante (il ne faut pas ovveride notre grille donc on en créé une nouvelle)
+    :param joueur: Contient quel est le joueur actuel : "O" ou "X"
+    :return: 1 / 0 / -1 en fonction de si le coup est bon ou non
+    """
     final_state = etat_final(actions)
     if final_state != -2:
         return final_state
@@ -65,7 +72,29 @@ def minmax(actions, joueur):
         return meilleur_valeur
 
 
+def affichage(actions):
+    """
+    Question 4
+    Fonction permettant d'afficher la grille du morpion
+    :param actions: Contient l'état actuel de la grille de la partie
+    :return: none
+    """
+    print(actions[0] + ' | ' + actions[1] + ' | ' + actions[2])
+    print("----------")
+    print(actions[3] + ' | ' + actions[4] + ' | ' + actions[5])
+    print("----------")
+    print(actions[6] + ' | ' + actions[7] + ' | ' + actions[8])
+    print("\n")
+
+
 def trouverAction(actions, joueur):
+    """
+    Question 5
+    Permet de définir quel est la meilleure action à jouer pour le joueur courant
+    :param actions: Contient l'état actuel de la grille de la partie
+    :param joueur: Contient le joueur qui doit jouer
+    :return: L'indice du meilleur coup à jouer
+    """
     meilleure_valeur = -10000 if joueur == "O" else 10000
     meilleure_action = None
     for i in range(len(actions)):
@@ -73,6 +102,7 @@ def trouverAction(actions, joueur):
             nouvelles_actions = list(actions)
             nouvelles_actions[i] = joueur
             valeur = minmax(nouvelles_actions, "X" if joueur == "O" else "O")
+            print("valeur", valeur)
             if valeur > meilleure_valeur and joueur == "O":
                 meilleure_valeur = valeur
                 meilleure_action = i
@@ -82,70 +112,78 @@ def trouverAction(actions, joueur):
     return meilleure_action
 
 
-def affichage(actions):
-    # question 4
-    print(actions[0] + ' | ' + actions[1] + ' | ' + actions[2])
-    print("----------")
-    print(actions[3] + ' | ' + actions[4] + ' | ' + actions[5])
-    print("----------")
-    print(actions[6] + ' | ' + actions[7] + ' | ' + actions[8])
-    print("\n")
-
-
 # Boucle de jeu 2 ordinateurs
-grille = ["_"] * 9
-joueur_actuel = "O"
+def computers_fight():
+    """
+    Question 6
+    Boucle de jeu permetttant de faire s'affronter un ordinateur contre un autre ordinateur et
+     déclaré le vainqueur ou l'égalité entre eux.
+    :return: none
+    """
+    grille = ["_"] * 9
+    joueur_actuel = "O"  # représente le joueur max qui commence toujours en premier
 
-while True:
-    print(f"C'est au tour de {joueur_actuel} : ")
-    coup = trouverAction(grille, joueur_actuel)
-    grille[coup] = joueur_actuel
-    affichage(grille)
-    if etat_final(grille) != -2:
-        break
-    joueur_actuel = "X" if joueur_actuel == "O" else "O"
-
-# Afficher le résultat final
-affichage(grille)
-resultat = etat_final(grille)
-if resultat == 0:
-    print("Match nul.")
-elif resultat == 1:
-    print("O a gagné.")
-elif resultat == -1:
-    print("X a gagné.")
-
-"""
-# Boucle de jeu Clément vs ordinateur
-grille = ["_"] * 9
-joueur_actuel = "O"
-
-while True:
-    if joueur_actuel == "X":
-        # Tour de l'utilisateur
-        print("C'est à votre tour de jouer (X).")
+    while etat_final(grille) == -2:
+        print(f"C'est au tour de {joueur_actuel} : ")
+        coup = trouverAction(grille, joueur_actuel)
+        grille[coup] = joueur_actuel
         affichage(grille)
-        coup = int(input("Entrez le numéro de la case où vous voulez jouer (0-8): "))
-        grille[coup] = "X"
-        if etat_final(grille) != -2:
-            break
-        joueur_actuel = "O"
-    else:
-        # Tour de l'ordinateur
-        print("C'est au tour de l'ordinateur (O).")
-        coup = trouverAction(grille, "O")
-        grille[coup] = "O"
-        if etat_final(grille) != -2:
-            break
-        joueur_actuel = "X"
+        joueur_actuel = "X" if joueur_actuel == "O" else "O"
 
-# Afficher le résultat final
-affichage(grille)
-resultat = etat_final(grille)
-if resultat == 0:
-    print("Match nul.")
-elif resultat == 1:
-    print("O a gagné.")
-elif resultat == -1:
-    print("X a gagné.")
-"""
+    # Afficher le résultat final
+    affichage(grille)
+    resultat = etat_final(grille)
+    if resultat == 0:
+        print("Match nul")
+    elif resultat == 1:
+        print("le gagnant est 0")
+    elif resultat == -1:
+        print("le gagnant est X")
+
+
+def player_vs_computer():
+    """
+    Question 7
+    Boucle de jeu permetttant de faire s'affronter un joueur contre un ordinateur et
+     déclaré le vainqueur ou l'égalité entre eux.
+    :return: none
+    """
+    grille = ["_"] * 9
+    joueur_actuel = "O"
+
+    while etat_final(grille) == -2:
+        if joueur_actuel == "X":
+            # Tour de l'utilisateur : joueur min
+            print("C'est à votre tour de jouer (X).")
+            affichage(grille)
+
+            coup = None
+            while coup is None:
+                coup = int(input("Entrez le numéro de la case où vous voulez jouer (0-8): "))
+                if coup > 8:
+                    print("Veuillez saisir un nombre entre 0 et 8.")
+                    coup = None
+                elif grille[coup] != "_":
+                    print("Case déjà remplie, veuillez choisir une autre case.")
+                    coup = None
+
+            grille[coup] = "X"
+        else:
+            # Tour de l'ordinateur : joueur max
+            print("C'est au tour de l'ordinateur (O).")
+            coup = trouverAction(grille, "O")
+            grille[coup] = "O"
+        joueur_actuel = "X" if joueur_actuel == "O" else "O"
+
+    # Afficher le résultat final
+    affichage(grille)
+    resultat = etat_final(grille)
+    if resultat == 0:
+        print("Match nul")
+    elif resultat == 1:
+        print("le gagnant est 0")
+    elif resultat == -1:
+        print("le gagnant est X")
+
+
+computers_fight()  # permet de lancer la simulation de combat entre les deux ordinateurs
